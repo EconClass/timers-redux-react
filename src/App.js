5d50b9abf808bbab3ab5f592cbaa5adad9417206
EconClass <bizmail327@gmail.com>
2019-09-24 14:37:08 -0700

    [add] timer interactions

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
 import reducers from './reducers';
+import NewTimer from './components/new-timer';
+import ListTimers from './components/list-timers';
 
 const store = createStore(reducers)
 
@@ -14,9 +16,8 @@ function App() {
         <header className="App-header">
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          Tmrz
-          </p>
+        <NewTimer />
+        <ListTimers />
       </div>
     </Provider>
   );
